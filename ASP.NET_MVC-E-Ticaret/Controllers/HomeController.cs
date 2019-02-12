a9af93f755dc0649a68648ba234659bf3233cafd
Mücahit Seba <mucahitseba@gmail.com>
2019-02-12 22:30:47 +0200

    ASP.NET MVC E-Ticaret çalışması.

---
+++ ASP.NET_MVC-E-Ticaret/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using ASP.NET_MVC_E_Ticaret.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP.NET_MVC_E_Ticaret.Controllers
+{
+    public class HomeController : Controller
+    {
+        MyContext _context = new MyContext();
+        public ActionResult Index()
+        {
+            var products = _context.Products.Where(i => i.isApproved == true && i.isHome == true).ToList();
+            return View(products);
+        }
+    }
+}
---
+++ ASP.NET_MVC-E-Ticaret/Models/EduraInitializer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ASP.NET_MVC_E_Ticaret.Models
+{
+    public class EduraInitializer:DropCreateDatabaseAlways<MyContext>
+    {
+        protected override void Seed(MyContext context)
+        {
+            List<Category> kategoriler = new List<Category>()
+            {
+                new Category(){Name="Furniture"},
+                new Category(){Name="Electronics"},
+                new Category(){Name="Books"},
+                new Category(){Name="Accessories"},
+                new Category(){Name="Computers"},
+                new Category(){Name="Camera"}
+            };
+            foreach (var item in kategoriler)
+            {
+                context.Categories.Add(item);
+            }
+            context.SaveChanges();
+
+            List<Product> urunler = new List<Product>()
+            {
+                new Product(){Name="Photo Camera",Image="product1.jpg",Description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget leo at velit imperdiet varius. In eu ipsum vitae velit congue iaculis vitae at risus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vitae vehicula enim. Sed quis ante quis eros maximus dignissim a eu mi. Proin varius arcu metus.",isApproved=true,isFeatured=true,isHome=true,Price=49,CategoryId=6},
+                new Product(){Name="Comfortable Sofa",Image="product3.jpg",Description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget leo at velit imperdiet varius. In eu ipsum vitae velit congue iaculis vitae at risus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vitae vehicula enim. Sed quis ante quis eros maximus dignissim a eu mi. Proin varius arcu metus.",isApproved=true,isFeatured=true,isHome=true,Price=79,CategoryId=1},
+                new Product(){Name="Hand Bag",Image="product4.jpg",Description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget leo at velit imperdiet varius. In eu ipsum vitae velit congue iaculis vitae at risus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vitae vehicula enim. Sed quis ante quis eros maximus dignissim a eu mi. Proin varius arcu metus.",isApproved=true,isFeatured=true,isHome=true,Price=99,CategoryId=3},
+                new Product(){Name="Photo Camera",Image="product1.jpg",Description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget leo at velit imperdiet varius. In eu ipsum vitae velit congue iaculis vitae at risus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vitae vehicula enim. Sed quis ante quis eros maximus dignissim a eu mi. Proin varius arcu metus.",isApproved=true,isFeatured=true,isHome=true,Price=49,CategoryId=1},
+                new Product(){Name="Comfortable Sofa",Image="product3.jpg",Description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget leo at velit imperdiet varius. In eu ipsum vitae velit congue iaculis vitae at risus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vitae vehicula enim. Sed quis ante quis eros maximus dignissim a eu mi. Proin varius arcu metus.",isApproved=true,isFeatured=true,isHome=true,Price=79,CategoryId=2},
+                new Product(){Name="Hand Bag",Image="product4.jpg",Description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget leo at velit imperdiet varius. In eu ipsum vitae velit congue iaculis vitae at risus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vitae vehicula enim. Sed quis ante quis eros maximus dignissim a eu mi. Proin varius arcu metus.",isApproved=true,isFeatured=true,isHome=false,Price=99,CategoryId=1},
+                new Product(){Name="Photo Camera",Image="product1.jpg",Description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget leo at velit imperdiet varius. In eu ipsum vitae velit congue iaculis vitae at risus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vitae vehicula enim. Sed quis ante quis eros maximus dignissim a eu mi. Proin varius arcu metus.",isApproved=true,isFeatured=true,isHome=true,Price=49,CategoryId=3},
+                new Product(){Name="Comfortable Sofa",Image="product3.jpg",Description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget leo at velit imperdiet varius. In eu ipsum vitae velit congue iaculis vitae at risus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vitae vehicula enim. Sed quis ante quis eros maximus dignissim a eu mi. Proin varius arcu metus.",isApproved=true,isFeatured=true,isHome=false,Price=79,CategoryId=4},
+                new Product(){Name="Hand Bag",Image="product4.jpg",Description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget leo at velit imperdiet varius. In eu ipsum vitae velit congue iaculis vitae at risus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vitae vehicula enim. Sed quis ante quis eros maximus dignissim a eu mi. Proin varius arcu metus.",isApproved=true,isFeatured=true,isHome=false,Price=99,CategoryId=4},
+                new Product(){Name="Photo Camera",Image="product1.jpg",Description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget leo at velit imperdiet varius. In eu ipsum vitae velit congue iaculis vitae at risus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vitae vehicula enim. Sed quis ante quis eros maximus dignissim a eu mi. Proin varius arcu metus.",isApproved=true,isFeatured=true,isHome=true,Price=49,CategoryId=6},
+                new Product(){Name="Comfortable Sofa",Image="product3.jpg",Description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget leo at velit imperdiet varius. In eu ipsum vitae velit congue iaculis vitae at risus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vitae vehicula enim. Sed quis ante quis eros maximus dignissim a eu mi. Proin varius arcu metus.",isApproved=true,isFeatured=true,isHome=false,Price=79,CategoryId=2},
+                new Product(){Name="Hand Bag",Image="product4.jpg",Description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget leo at velit imperdiet varius. In eu ipsum vitae velit congue iaculis vitae at risus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vitae vehicula enim. Sed quis ante quis eros maximus dignissim a eu mi. Proin varius arcu metus.",isApproved=true,isFeatured=true,isHome=true,Price=99,CategoryId=1}
+            };
+
+            foreach (var item in urunler)
+            {
+                context.Products.Add(item);
+            }
+            context.SaveChanges();
+
+            base.Seed(context);
+        }
+    }
+}
